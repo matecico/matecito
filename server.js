f2fc875507dda8f103f5fc58b3a99d9e9a836bd1
matecico <matecico15@gmail.com>
2026-08-09 02:12:49 -0300

    URL final matecico.netlify.app

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const TELEGRAM_CHAT_ID = '7691781211';
 // URL del Backend en Render (para Webhooks de Mercado Pago)
 const URL_PUBLICA_SERVER = 'https://matecito.onrender.com';
 
-// URL del Frontend en Netlify (ACTUALIZADA)
-const URL_FRONTEND = 'https://mmatecico.netlify.app';
+// URL del Frontend en Netlify (OFICIAL)
+const URL_FRONTEND = 'https://matecico.netlify.app';
 
 const client = new MercadoPagoConfig({ accessToken: MP_ACCESS_TOKEN });
 
